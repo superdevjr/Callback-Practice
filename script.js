911c8fa5b28d66932723102364719c6417492a0f
jhylacey <jhylacey@proton.me>
2023-03-24 13:31:36 -0500

    Callback hell

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-let pizza
+//first way -----------------
 
-function orderPizza() {
-    console.log("Order Pizza")
+// let pizza
+
+// function orderPizza() {
+//     console.log("Order Pizza")
+//     setTimeout(() => {
+//         pizza = "a pizza emoji";
+//         console.log('pizza is ready')
+
+//     }, 2000);
+//     console.log(`${pizza} was ordered`)
+// }
+
+
+// orderPizza()
+// console.log("call my bro")
+// console.log(`eat ${pizza}`)
+
+
+// second way ------------
+
+function orderPizza(cb) {
     setTimeout(() => {
-        pizza = "a pizza emoji";
-        console.log('pizza is ready')
+        const pizza = `aTastyPizza`
+        cb(pizza)
+    }, 2000)
 
-    }, 2000);
-    console.log(`${pizza} was ordered`)
 }
 
+function pizzaReady(pizza) {
+    console.log(`eat the ${pizza}`)
+}
 
-orderPizza()
-console.log("call my bro")
-console.log(`eat ${pizza}`)
+orderPizza(pizzaReady)
+
+console.log('Call qoli')
